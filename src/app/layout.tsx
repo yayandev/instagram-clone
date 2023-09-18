3ac12b91c8c78f240ca5c18e6d122b2a052cb979
yayanfr20 <yayanfathurohman20@gmail.com>
2023-09-19 06:32:35 +0700

    setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Instagram",
+  description: "Instagram Clone by Yayan Faturrohman",
+  openGraph: {
+    type: "website",
+    locale: "id_ID",
+    url: "https://www.instagram.com/",
+    title: "Instagram Clone",
+    description: "Instagram Clone by Yayan Faturrohman",
+    images: [
+      {
+        url: "/favicon-32x32.png",
+      },
+    ],
+  },
+  authors: [
+    {
+      name: "Yayan Faturrohman",
+      url: "https://github.com/yayanfr20",
+    },
+  ],
+  manifest: "/site.webmanifest",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>INSTAGRAM CLONE</h1>
+    </main>
+  );
+}
