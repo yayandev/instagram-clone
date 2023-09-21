c112c961f8d685d37a2280ba8f114db24ff19ce4
yayanfr20 <yayanfathurohman20@gmail.com>
2023-09-22 00:56:56 +0700

    fixs error in section suggestions

---
+++ src/components/stories/StoriesDemo.tsx
@@ -0,0 +1,232 @@
+import Link from "next/link";
+
+const StoriesDemo = () => {
+  return (
+    <div className="flex gap-3 items-center">
+      {/* <button className="text-gray-500">&lt;</button> */}
+      <div className="p-3  w-full overflow-x-scroll scroll-smooth scroll-hide">
+        <div className="flex space-x-4">
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+          <Link href={"#"}>
+            <div
+              className={`w-16 h-16 flex-shrink-0 rounded-full overflow-hidden border-2 ${"border-gray-200"}`}
+            >
+              <img
+                src={"/default-pp.png"}
+                alt={"user"}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="text-xs text-center">user</p>
+          </Link>
+        </div>
+      </div>
+      {/* <button className="text-gray-500">&gt;</button> */}
+    </div>
+  );
+};
+
+export default StoriesDemo;
